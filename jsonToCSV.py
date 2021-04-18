6f1050bbb35b81823450cf25ac59e959a26c722c
CDP <cdp@localhost.localdomain>
2021-04-18 19:34:29 +0300

    Fixed an issue that would cause every row to have the same type of information (like a column)

--- jsonToCSV.py
+++ jsonToCSV.py
@@ -9,18 +9,18 @@ class App(QWidget):
 
     def __init__(self):
         super().__init__()
-        self.title = 'PyQt5 file dialogs - pythonspot.com'
-        self.left = 10
-        self.top = 10
-        self.width = 640
-        self.height = 480
+        self.title = 'Convert from .json to .csv'
+        self.left = 0
+        self.top = 0
+        self.width = 320
+        self.height = 120
         self.initUI()
     
     def initUI(self):
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
 
-        button = QPushButton('PyQt5 button', self)
+        button = QPushButton('Select a file', self)
         button.setToolTip('This is an example button')
         button.move(100,70)
         button.clicked.connect(self.on_click)
@@ -29,17 +29,17 @@ class App(QWidget):
 
     @pyqtSlot()
     def on_click(self):
-        print('PyQt5 button click')
+        print('button click')
         self.openFileNameDialog()
     
     def openFileNameDialog(self):
         options = QFileDialog.Options()
         options |= QFileDialog.DontUseNativeDialog
-        fileName, _ = QFileDialog.getOpenFileName(self,"QFileDialog.getOpenFileName()", "","JSON Files (*.json);;", options=options)
+        fileName, _ = QFileDialog.getOpenFileName(self,"Select a file", "","JSON Files (*.json);;", options=options)
         
         #check if there is a file
         if fileName:
-            print(fileName)
+            #print(fileName)
             # extract the data from the JSON file
             with open(fileName, "r") as jayson:
                 data = json.load(jayson)
@@ -55,22 +55,22 @@ class App(QWidget):
             if entries == 1:
                 file_name = info
 
+# do not CHANGE!!!!!!!
                 for info in data[file_name][0]:
                     #print(info)
                     titles.append(info)
 
-                #print(titles)
-
-                index = 0
-
+                rows.append([])
                 for key in titles:
                     rows.append([])
-                    for info in data[file_name]:
-                        #print(info)
-                        rows[index].append(info[key])
-                    index +=1
+# THIS ^ ^ ^ ^ ^^ ^ ^ ^^ ^ ^^^ ^^ ^ ^^ ^^ ^ 
 
-                #print(f"the rows are: {rows}")
+                for info in data[file_name]:
+                    index = 0
+                    for key in titles:
+                        rows[index].append(info[key])
+                    index += 1 # it stays here, because the rows array shows us how many categories(keys) we have
+                
                 with open(str(file_name) + ".csv", "w") as csvfile:
                     csvwriter = csv.writer(csvfile)
                     csvwriter.writerow(titles)
@@ -88,13 +88,11 @@ class App(QWidget):
                     for info in data:
                         rows[index].append(info[key])
                     index += 1
-                    
+
                 with open(str(file_name) + ".csv", "w") as csvfile:
                     csvwriter = csv.writer(csvfile)
                     csvwriter.writerow(titles)
                     csvwriter.writerows(rows)
-
-            # TODO extract the data and write it with commas!
 
 
 if __name__ == '__main__':
